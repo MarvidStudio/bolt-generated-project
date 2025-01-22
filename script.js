e261ad2bc574a1ea7b5327f4d4b9d66a3c6fb887
MarvidStudio <alfeev.oleg@yandex.ru>
2025-01-23 00:44:15 +0300

    Initial commit from your app

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-document.getElementById('logoContainer').addEventListener('click', function() {
+let currentButtonId = null;
+
+    function openEditMenu(buttonId) {
+      currentButtonId = buttonId;
+      const button = document.getElementById(buttonId);
+      const buttonLink = button.querySelector('.button-link');
+      
+      document.getElementById('buttonText').value = buttonLink.textContent;
+      document.getElementById('buttonLink').value = buttonLink.href;
+
+      document.getElementById('sideMenu').classList.add('active');
+    }
+
+    document.getElementById('saveButton').addEventListener('click', function() {
+      const buttonText = document.getElementById('buttonText').value;
+      const buttonLink = document.getElementById('buttonLink').value;
+
+      const button = document.getElementById(currentButtonId);
+      const buttonLinkElement = button.querySelector('.button-link');
+
+      buttonLinkElement.textContent = buttonText;
+      buttonLinkElement.href = buttonLink;
+
+      document.getElementById('sideMenu').classList.remove('active'); // Hide side menu
+    });
+
+    document.getElementById('logoContainer').addEventListener('click', function() {
       document.getElementById('logoUpload').click();
     });
 
